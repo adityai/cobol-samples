b6cedd41682cadf0c3200121f3464a1d7291904f
Aditya Inapurapu <aialerts@yahoo.com>
2018-05-06 15:04:00 -0700

    Initial commit Hello.cbl

---
+++ Hello.cbl
@@ -0,0 +1,17 @@
+      ******************************************************************
+      * Author: Aditya Inapurapu
+      * Date: 5/6/2018
+      * Purpose: Sample hello world
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Hello.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            STOP RUN.
+       END PROGRAM Hello.
+
